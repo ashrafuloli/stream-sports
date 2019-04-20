2a1cabd3bc129478dccf0f6d092ce6420728efff
Ashraful Islam Oli <rock.ashraful@gmail.com>
2019-04-21 03:35:07 +0600

    fixed menu

--- functions.php
+++ functions.php
@@ -224,4 +224,70 @@ function stream_sports_remove_attachment_with_post($post_id)
 add_action('before_delete_post', 'stream_sports_remove_attachment_with_post', 10);
 
 
+function stream_sports_register_cpt_sport()	
+{	
+ 	/**	
+	 * Post Type: Sports.	
+	 */	
+ 	$labels = array(	
+		"name" => __("Sports", "stream-sports"),	
+		"singular_name" => __("Sport", "stream-sports"),	
+	);	
+ 	$args = array(	
+		"label" => __("Sports", "stream-sports"),	
+		"labels" => $labels,	
+		"description" => "",	
+		"public" => true,	
+		"publicly_queryable" => true,	
+		"show_ui" => true,	
+		"delete_with_user" => false,	
+		"show_in_rest" => true,	
+		"rest_base" => "",	
+		"rest_controller_class" => "WP_REST_Posts_Controller",	
+		"has_archive" => "sports",	
+		"show_in_menu" => true,	
+		"show_in_nav_menus" => true,	
+		"exclude_from_search" => false,	
+		"capability_type" => "post",	
+		"map_meta_cap" => true,	
+		"hierarchical" => false,	
+		"rewrite" => array("slug" => "sport", "with_front" => true),	
+		"query_var" => true,	
+		"menu_position" => 5,	
+		"menu_icon" => "dashicons-sos",	
+		"supports" => array("title", "editor", "thumbnail"),	
+	);	
+ 	register_post_type("sport", $args);	
+}	
+ add_action('init', 'stream_sports_register_cpt_sport');	
+
+ function stream_sports_register_taxes_cpt_sport()	
+{	
+ 	/**	
+	 * Taxonomy: Sports Categories.	
+	 */	
+ 	$labels = array(	
+		"name" => __("Sports Categories", "stream-sports"),	
+		"singular_name" => __("Sport Category", "stream-sports"),	
+	);	
+ 	$args = array(	
+		"label" => __("Sports Categories", "stream-sports"),	
+		"labels" => $labels,	
+		"public" => true,	
+		"publicly_queryable" => true,	
+		"hierarchical" => true,	
+		"show_ui" => true,	
+		"show_in_menu" => true,	
+		"show_in_nav_menus" => true,	
+		"query_var" => true,	
+		"rewrite" => array('slug' => 'sport_category', 'with_front' => true,),	
+		"show_admin_column" => true,	
+		"show_in_rest" => true,	
+		"rest_base" => "sport_category",	
+		"rest_controller_class" => "WP_REST_Terms_Controller",	
+		"show_in_quick_edit" => false,	
+	);	
+	register_taxonomy("sport_category", array("sport"), $args);	
+}	
+ add_action('init', 'stream_sports_register_taxes_cpt_sport');
 
